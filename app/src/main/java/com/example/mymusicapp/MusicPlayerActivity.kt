9beb561b4de7c81cf40f32d4b936a6078ee01dca
GiorgiDolidzee <giorgi.dolidze.2@btu.edu.ge>
2021-01-08 17:49:27 +0400

    meotxe

---
+++ app/src/main/java/com/example/mymusicapp/MusicPlayerActivity.kt
@@ -0,0 +1,211 @@
+package com.example.mymusicapp
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.SeekBar
+import android.widget.TextView
+
+class MusicPlayerActivity : AppCompatActivity() {
+
+    private lateinit var mp : MediaPlayer
+
+    private var totalTime: Int = 0
+    private lateinit var playBtn: Button
+    private lateinit var volumeBar: SeekBar
+    private lateinit var positionBar: SeekBar
+
+    private lateinit var backFromMusic: ImageView
+
+    private lateinit var elapsedTimeLabel : TextView
+    private lateinit var remainingTimeLabel: TextView
+
+    private lateinit var musicImage: ImageView
+    private lateinit var musicName: TextView
+    private lateinit var musicArtist: TextView
+    private lateinit var musicBackImage: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_music_player)
+        supportActionBar?.hide()
+
+        musicImage = findViewById(R.id.musicImageView)
+        musicName = findViewById(R.id.musicNameText)
+        musicArtist = findViewById(R.id.musicArtistText)
+        musicBackImage = findViewById(R.id.backImage)
+
+        val musname = intent.extras?.getString("value")
+        if (musname == "loseyourself") {
+            backFromMusic()
+            mp = MediaPlayer.create(this, R.raw.loseyourselfmusic)
+            musicImage.setBackgroundResource(R.drawable.loseyourselfphoto)
+            musicBackImage.setBackgroundResource(R.drawable.ukana)
+            musicName.text = "Lose Yourself"
+            musicArtist.text = "Eminem"
+
+        } else if (musname == "taste") {
+            backFromMusic()
+            mp = MediaPlayer.create(this, R.raw.taste)
+            musicImage.setBackgroundResource(R.drawable.tastephoto)
+            musicBackImage.setBackgroundResource(R.drawable.ukanataste)
+            musicName.text = "Taste"
+            musicArtist.text = "Tyga"
+
+        } else if (musname == "droptheworld") {
+            backFromMusic()
+            mp = MediaPlayer.create(this, R.raw.droptheworld)
+            musicImage.setBackgroundResource(R.drawable.droptheworldphoto)
+            musicBackImage.setBackgroundResource(R.drawable.ukanadroptheworld)
+            musicName.text = "Drop The World"
+            musicArtist.text = "Lil Wayne ft. Eminem"
+
+        } else if (musname == "alleyezonme") {
+            backFromMusic()
+            mp = MediaPlayer.create(this, R.raw.alleyezonme)
+            musicImage.setBackgroundResource(R.drawable.alleyezonmephoto)
+            musicBackImage.setBackgroundResource(R.drawable.ukanaalleyezonme)
+            musicName.text = "All Eyez On Me"
+            musicArtist.text = "2 Pac"
+        }
+        
+
+        playBtn = findViewById(R.id.playBtn)
+        volumeBar = findViewById(R.id.volumeBar)
+        positionBar = findViewById(R.id.positionBar)
+
+        elapsedTimeLabel = findViewById(R.id.elapsedTimeLabel)
+        remainingTimeLabel = findViewById(R.id.remainingTimeLabel)
+
+        mp.isLooping = true
+        mp.setVolume(0.5f, 0.5f)
+        totalTime = mp.duration
+
+        // Volume bar
+        volumeBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                )
+                {
+                    if (fromUser) {
+                        var volumeNum = progress / 50.0f
+                        mp.setVolume(volumeNum, volumeNum)
+                    }
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+                }
+            }
+        )
+
+        // Position Bar
+        positionBar.max = totalTime
+        positionBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    if (fromUser) {
+                        mp.seekTo(progress)
+                    }
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                }
+            }
+        )
+
+        // Thread
+        Thread(Runnable {
+            while (mp != null) {
+                try {
+                    var msg = Message()
+                    msg.what = mp.currentPosition
+                    handler.sendMessage(msg)
+                    Thread.sleep(1000)
+                } catch (e: InterruptedException) {
+
+                }
+            }
+        }).start()
+
+    }
+
+    @SuppressLint("HandlerLeak")
+    var handler = object : Handler() {
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+            var currentPosition = msg.what
+
+            // Update positionbar
+            positionBar.progress = currentPosition
+
+            // Update labels
+            var elapsedTime = createTimeLabel(currentPosition)
+            elapsedTimeLabel.text = elapsedTime
+
+            var remainingTime = createTimeLabel(totalTime - currentPosition)
+            remainingTimeLabel.text = "-$remainingTime"
+
+        }
+    }
+
+    fun createTimeLabel(time: Int): String {
+        var timeLabel = ""
+        var min = time / 1000 / 60
+        var sec = time / 1000 % 60
+
+        timeLabel = "$min:"
+        if (sec < 10) timeLabel += "0"
+        timeLabel += sec
+
+        return timeLabel
+
+    }
+
+
+    fun playBtnClick(v: View) {
+
+        if (mp.isPlaying) {
+            // stop
+
+            mp.pause()
+            playBtn.setBackgroundResource(R.drawable.playicon)
+        } else {
+            // Start
+
+            mp.start()
+            playBtn.setBackgroundResource(R.drawable.pauseicon)
+        }
+
+    }
+
+    fun backFromMusic() {
+        backFromMusic = findViewById(R.id.backFromMusic)
+        backFromMusic.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+            mp.stop()
+        }
+    }
+
+}
--- app/src/main/java/com/example/mymusicapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/mymusicapp/fragments/HomeFragment.kt
@@ -9,45 +9,72 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import com.example.mymusicapp.EminemActivity
-import com.example.mymusicapp.LoseyourselfActivity
-import com.example.mymusicapp.R
-import com.example.mymusicapp.TasteActivity
+import com.example.mymusicapp.*
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private lateinit var navController: NavController
+
     private lateinit var eminemImageButton: ImageButton
+    private lateinit var twopacImageButton: ImageButton
+
     private lateinit var loseYourSelf: ImageButton
     private lateinit var taste: ImageButton
+    private lateinit var dropwtheworld: ImageButton
+    private lateinit var alleyezonme: ImageButton
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         (requireActivity() as AppCompatActivity).supportActionBar?.hide()
-
         navController = Navigation.findNavController(view)
 
-        eminemImageButton = view.findViewById(R.id.eminemPhoto)
+        var musicname = ""
 
+        // • TOP Artist List •
+        eminemImageButton = view.findViewById(R.id.eminemImageButton)
         eminemImageButton.setOnClickListener {
-            val intent = Intent (activity, EminemActivity::class.java)
-            activity?.startActivity(intent)
+            startActivity(Intent(activity, EminemActivity::class.java))
         }
 
+        twopacImageButton = view.findViewById(R.id.twopacImageButton)
+        twopacImageButton.setOnClickListener {
+            startActivity(Intent(activity, EminemActivity::class.java))
+        }
+
+
+        // • Music List •
         loseYourSelf = view.findViewById(R.id.eminemLoveYourself)
         loseYourSelf.setOnClickListener {
-            val loseyourself = Intent (activity, LoseyourselfActivity::class.java)
-            activity?.startActivity(loseyourself)
-
+            musicname = "loseyourself"
+            val myActivity = (Intent(activity, MusicPlayerActivity::class.java))
+            myActivity.putExtra("value", musicname)
+            activity?.startActivity(myActivity)
         }
 
         taste = view.findViewById(R.id.tygaTaste)
         taste.setOnClickListener {
-            val taste = Intent (activity, TasteActivity::class.java)
-            activity?.startActivity(taste)
+            musicname = "taste"
+            val myActivity = (Intent(activity, MusicPlayerActivity::class.java))
+            myActivity.putExtra("value", musicname)
+            activity?.startActivity(myActivity)
+        }
+
+        dropwtheworld = view.findViewById(R.id.lilwayneDropTheWorld)
+        dropwtheworld.setOnClickListener {
+            musicname = "droptheworld"
+            val myActivity = (Intent(activity, MusicPlayerActivity::class.java))
+            myActivity.putExtra("value", musicname)
+            activity?.startActivity(myActivity)
+        }
+
+        alleyezonme = view.findViewById(R.id.twopacAllEyez)
+        alleyezonme.setOnClickListener {
+            musicname = "alleyezonme"
+            val myActivity = (Intent(activity, MusicPlayerActivity::class.java))
+            myActivity.putExtra("value", musicname)
+            activity?.startActivity(myActivity)
         }
 
     }
-
 
 }
